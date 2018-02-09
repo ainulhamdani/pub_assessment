09d8edd6b01962f1118cec181e4546eed47c2fdb
AinulHamdani <hamdani.ainul@gmail.com>
2018-02-09 14:23:47 +0800

    update graph api to 2.8
    Signed-off-by: AinulHamdani <hamdani.ainul@gmail.com>

--- application/views/login.php
+++ application/views/login.php
@@ -2,13 +2,15 @@
 $this->load->view("header-login");
 ?>
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=1722550147790150';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+<script>
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
 <div class="row justify-content-center">
   <div class="col-lg-3 col-md-6 col-sm-6">
     <form class="form-signin" method="post" action="<?=site_url('welcome/login')?>">
@@ -38,7 +40,7 @@ $this->load->view("header-login");
 <div class="row"><hr></div>
 <div class="row justify-content-center">
   <div class="col-lg-3 col-md-6 col-sm-6">
-    <center><fb:login-button scope="public_profile,email" class="fb-login-button" onlogin="onLogIn()" data-width="250" data-height="50" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-use-continue-as="false"></fb:login-button></center>
+    <center><div class="fb-login-button" data-max-rows="1" onlogin="onLogIn()" data-size="large" data-button-type="login_with" data-width="250" data-scope="public_profile, email"></div></center>
   </div>
 </div>
 <div class="row"><hr></div>
@@ -67,7 +69,8 @@ $this->load->view("header-login");
           sessionStorage.setItem("imageurl", profile.getImageUrl());
           sessionStorage.setItem("email", profile.getEmail());
           sessionStorage.setItem("token", googleUser.getAuthResponse().id_token);
-          $(location).attr("href","<?=base_url()?>welcome/register");
+          window.location.href = "<?=base_url()?>welcome/register";
+          // $(location).attr("href","<?=base_url()?>welcome/register");
         } else {
           alert("Sorry! No Web Storage support..");
         }
@@ -85,44 +88,58 @@ $this->load->view("header-login");
   // Facebook login
   function onLogIn() {
     FB.getLoginStatus(function(response) {
-      if (response.status === 'connected') {
-        var fb_token = response.authResponse.accessToken;
-        FB.api('/me?fields=id,link,gender,picture,name,email', function(response) {
-          var data = {
-            id:response.id,
-            fullname:response.name,
-            imageurl:response.picture.data.url,
-            email:response.email,
-            token:fb_token
-          };
-          $.post( "<?=base_url()?>welcome/fboauth2", data )
-          .done(function( data ) {
-            data = $.parseJSON(data);
-            if(data['code']==404){
-              if (typeof(Storage) !== "undefined") {
-                sessionStorage.setItem("source", "facebook");
-                sessionStorage.setItem("id", response.id);
-                sessionStorage.setItem("fullname", response.name);
-                sessionStorage.setItem("imageurl", response.picture.data.url);
-                sessionStorage.setItem("email", response.email);
-                sessionStorage.setItem("token", fb_token);
-                $(location).attr("href","<?=base_url()?>welcome/register");
-              } else {
-                alert("Sorry! No Web Storage support..");
-              }
-            }else if(data['code']==200){
-              $(location).attr("href","<?=base_url()?>");
-            }else if(data['code']==202){
-              $(location).attr("href","<?=base_url()?>");
-            }else{
-              alert("code 500");
-            }
-          }
-          );
-        });
-      } 
+      fblogin(response);
     });
   }
+
+  function fblogin(response){
+    if (response.status === 'connected') {
+      var fb_token = response.authResponse.accessToken;
+      FB.api('/me?fields=id,link,gender,picture,name,email', function(response) {
+        var data = {
+          id:response.id,
+          fullname:response.name,
+          imageurl:response.picture.data.url,
+          email:response.email,
+          token:fb_token
+        };
+        $.post( "<?=base_url()?>welcome/fboauth2", data )
+        .done(function( data ) {
+          data = $.parseJSON(data);
+          if(data['code']==404){
+            if (typeof(Storage) !== "undefined") {
+              sessionStorage.setItem("source", "facebook");
+              sessionStorage.setItem("id", response.id);
+              sessionStorage.setItem("fullname", response.name);
+              sessionStorage.setItem("imageurl", response.picture.data.url);
+              sessionStorage.setItem("email", response.email);
+              sessionStorage.setItem("token", fb_token);
+              $(location).attr("href","<?=base_url()?>welcome/register");
+            } else {
+              alert("Sorry! No Web Storage support..");
+            }
+          }else if(data['code']==200){
+            $(location).attr("href","<?=base_url()?>");
+          }else if(data['code']==202){
+            $(location).attr("href","<?=base_url()?>");
+          }else{
+            alert("code 500");
+          }
+        }
+        );
+      });
+    } 
+  }
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1722550147790150',
+      cookie     : true,
+      xfbml      : true,
+      status     : true,
+      version    : 'v2.8'
+    });
+  };
 </script>
 
 <?php
