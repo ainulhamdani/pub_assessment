9888e6c916342aa2657c90bf59eea89355e6b6f1
AinulHamdani <hamdani.ainul@gmail.com>
2018-02-13 00:10:12 +0800

    add notification when no task
    Signed-off-by: AinulHamdani <hamdani.ainul@gmail.com>

--- application/views/user/assess/grade.php
+++ application/views/user/assess/grade.php
@@ -7,6 +7,18 @@ $this->load->view("user/nav");
     </div>
     <hr>
     <div class="col-md-12">
+        <?php if($task==null){ ?>
+            <div class="row">
+                <div class="col-md-3 col-sm-12 col-12" style="min-height: 200px">
+                    <div class="card">
+                        <div class="card-header text-white bg-danger">No Task</div>
+                        <div class="card-body">
+                            There is no task yet. Please check back later<br><br>
+                        </div>
+                     </div>
+                 </div>
+            </div>
+        <?php }else{ ?>
     	<div class="row justify-content-md-center">
     		<div class="col-12">
 	    		<center><img src="<?=base_url()."asset/uploads/".$task->img_id?>" class="img-fluid" alt="Responsive image"></center>
@@ -20,6 +32,7 @@ $this->load->view("user/nav");
                 </div>
             </div>
         </div>
+        <?php } ?>
     </div>
 <?php
 $this->load->view("footer");
