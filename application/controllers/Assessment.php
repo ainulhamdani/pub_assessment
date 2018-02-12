9f029ec2f450744b4449a6f71710cd25ebf19ef9
AinulHamdani <hamdani.ainul@gmail.com>
2018-02-13 07:16:35 +0800

    increase image size limit
    Signed-off-by: AinulHamdani <hamdani.ainul@gmail.com>

--- application/controllers/Assessment.php
+++ application/controllers/Assessment.php
@@ -110,8 +110,8 @@ class Assessment extends CI_Controller {
             $config['file_name']            = $code.".jpg";
             $config['file_ext_tolower']     = TRUE;
             $config['max_size']             = 10000;
-            $config['max_width']            = 1024;
-            $config['max_height']           = 768;
+            $config['max_width']            = 2048;
+            $config['max_height']           = 2048;
 
             $this->load->library('upload');
             $this->upload->initialize($config);
