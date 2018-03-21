31e4e7809f758f823c2f4437d3f2176eca62fd7e
AinulHamdani <hamdani.ainul@gmail.com>
2018-03-21 12:34:04 +0800

    add register and login button on home

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -4,7 +4,6 @@ $this->load->view("admin/nav");
 ?>
 	<div class="page-header">
         <h1>BEATs Project</h1>
-        <h3>Platform Home Description</h3>
     </div>
     <hr>
     <div class="row">
@@ -17,6 +16,9 @@ $this->load->view("admin/nav");
 				            	<p>BEATs yang menjadi judul dari Project ini adalah akronim dari Beliefs, Emotions, Actions, and Thoughts (Kepercayaan, Emosi, Aksi, dan Pemikiran). Kami berargumen bahwa konektivitas dari kepercayaan, emosi, aksi, dan pemikiran yang mengalir dalam masyarakat melalui interaksi dan komunikasi membentuk sebuah kolektivitas yang kapabilitasnya melebihi dari sekedar kemampuan seseorang atau rata-rata kemampuan anggota komunitas. Untuk itu, secara garis besar project ini bertujuan untuk mengidentifikasi dan mengembangkan kecerdasan kolektif dalam komunitas masyarakat, seperti dusun dan desa, untuk membangun kemampuan kolaborasi masyarakat dalam menghadapi persoalan-persoalan kontekstual di wilayah tempat tinggalnya. </p>
 
 				            	<p>Hal ini dilakukan dengan melakukan beberapa kegiatan bersama dengan masyarakat untuk mengidentifikasi faktor-faktor yang membangun kecerdasan kolektif, seperti pemetaan pola aliran BEATs diantara anggota masyarakat, mengadakan tes sensitivitas sosial, melakukan kegiatan kreativitas, kegiatan pengambilan keputusan, kegiatan resolusi konflik, dan kegiatan koordinasi psikomotor. Identifikasi ini pun akan dilanjutkan dengan program implementasi pengembangan kolaborasi masyarakat dalam menghadapi dan memecahkan permasalahan kontekstual berdasarkan temuan yang ada.</p>
+
+				            	<a href="<?=base_url()?>welcome/register" class="btn btn-primary btn-block">REGISTER</a>
+				            	<a href="<?=base_url()?>welcome/login" class="btn btn-primary btn-block">LOGIN</a>
 				            </div>
 				         </div>
 				     </div>
