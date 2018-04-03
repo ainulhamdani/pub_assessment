b77cf531c69a3d75f964f028f8098d166cdaeb6d
AinulHamdani <hamdani.ainul@gmail.com>
2018-04-03 09:18:30 +0800

    fix register button still show up after login
    Signed-off-by: AinulHamdani <hamdani.ainul@gmail.com>

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -16,9 +16,10 @@ $this->load->view("admin/nav");
 				            	<p>BEATs yang menjadi judul dari Project ini adalah akronim dari Beliefs, Emotions, Actions, and Thoughts (Kepercayaan, Emosi, Aksi, dan Pemikiran). Kami berargumen bahwa konektivitas dari kepercayaan, emosi, aksi, dan pemikiran yang mengalir dalam masyarakat melalui interaksi dan komunikasi membentuk sebuah kolektivitas yang kapabilitasnya melebihi dari sekedar kemampuan seseorang atau rata-rata kemampuan anggota komunitas. Untuk itu, secara garis besar project ini bertujuan untuk mengidentifikasi dan mengembangkan kecerdasan kolektif dalam komunitas masyarakat, seperti dusun dan desa, untuk membangun kemampuan kolaborasi masyarakat dalam menghadapi persoalan-persoalan kontekstual di wilayah tempat tinggalnya. </p>
 
 				            	<p>Hal ini dilakukan dengan melakukan beberapa kegiatan bersama dengan masyarakat untuk mengidentifikasi faktor-faktor yang membangun kecerdasan kolektif, seperti pemetaan pola aliran BEATs diantara anggota masyarakat, mengadakan tes sensitivitas sosial, melakukan kegiatan kreativitas, kegiatan pengambilan keputusan, kegiatan resolusi konflik, dan kegiatan koordinasi psikomotor. Identifikasi ini pun akan dilanjutkan dengan program implementasi pengembangan kolaborasi masyarakat dalam menghadapi dan memecahkan permasalahan kontekstual berdasarkan temuan yang ada.</p>
-
+				            	<?php if(empty($this->session->userdata('id_user'))&&$this->session->userdata('user_valid') == FALSE) { ?>
 				            	<a href="<?=base_url()?>welcome/register" class="btn btn-primary btn-block">REGISTER</a>
 				            	<a href="<?=base_url()?>welcome/login" class="btn btn-primary btn-block">LOGIN</a>
+				            	<?php } ?>
 				            </div>
 				         </div>
 				     </div>
